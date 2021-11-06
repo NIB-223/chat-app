9de4d5a2bd01d79bfe89b4532bcb6ea7535da687
Drxl95 <robertu.dev@gmail.com>
2021-11-06 12:45:58 -0500

    added states for bacground and passes to chat page

--- components/Chat.js
+++ components/Chat.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default class Chat extends React.Component {
+    componentDidMount() {
+        //put  username in navigation bar (passes prop from start)
+        const name = this.props.route.params.name;
+
+        //change background color based on user's choice (prop passed from start)
+        const background = this.props.route.params.background;
+        this.props.navigation.setOptions({
+            title: `Welcome ${name}`,
+            headerStyle: {
+                backgroundColor: `${background}`,
+            },
+            headerTintColor: "#212224",
+        });
+    }
 
     render() {
-        let name = this.props.route.params.name; // OR ...
-        // let { name } = this.props.route.params;
-
-        this.props.navigation.setOptions({ title: name });
-
         return (
-            <View>
-                {/* Rest of the UI */}
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text>Hello Screen2!</Text>
             </View>
         );
-    };
+    }
 }
--- components/Start.js
+++ components/Start.js
@@ -1,98 +1,136 @@
 // import Button from '@restart/ui/esm/Button';
-import React, { Component } from 'react';
-import { View, ImageBackground, TextInput, StyleSheet, Button, Image, Text } from 'react-native';
-
+import React, { Component } from "react";
+import {
+    View,
+    ImageBackground,
+    TextInput,
+    StyleSheet,
+    Image,
+    Text,
+    TouchableOpacity,
+} from "react-native";
 
 
 export default class Start extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { text: '' };
+        this.state = { name: '', background: "" };
     }
 
     render() {
         return (
-            <ImageBackground style={styles.background} source={require('../assets/BackgroundImage.png')} resizeMode="cover">
-                <Text style={styles.appTitle}>{this.state.text}</Text>
-                <View style={styles.container2}>
-                    <View style={styles.container3}>
-                        <View>
+            <View style={styles.container}>
+                <ImageBackground style={styles.background}
+                    source={require('../assets/BackgroundImage.png')}
+                    resizeMode="cover"
+                >
+                    <Text style={styles.appTitle}>{this.state.text}</Text>
+
+                    <View style={styles.contentBox}>
+                        <View style={styles.textContainer}>
                             <Image style={styles.icon}
                                 source={require("../assets/icon.svg")}
                                 size={14}
                             />
 
-                            {/*user enters their name*/}
+                            {/*user enters their name, this sets the state of the name*/}
                             <TextInput style={styles.nameInput}
-                                onChangeText={(text) => this.setState({ text })}
+                                onChangeText={(text) => this.setState({ name: text })}
                                 value={this.state.text}
                                 placeholder=' Your Name'
                             />
                         </View>
-                        <Text>Choose Background Color:</Text>
-                        <View style={styles.circleContainer}>
 
+                        {/*user touches a button for color, we set the background state to that color*/}
+                        <View>
+                            <Text style={styles.colorText}>Choose Background Color:</Text>
+                            <View style={styles.backgroundColorChoices}>
+                                <TouchableOpacity
+                                    style={styles.circle1}
+                                    onPress={() => this.setState({ background: "#090C08" })}
+                                />
+                                <TouchableOpacity
+                                    style={styles.circle2}
+                                    onPress={() => this.setState({ background: "#474056" })}
+                                />
+                                <TouchableOpacity
+                                    style={styles.circle3}
+                                    onPress={() => this.setState({ background: "#8A96A5" })}
+                                />
+                                <TouchableOpacity
+                                    style={styles.circle4}
+                                    onPress={() => this.setState({ background: "#B9C6AE" })}
+                                />
+                            </View>
                         </View>
-                        {/* takes user to chat room*/}
-                        <Button
+
+                        {/* takes user to chat room, pass the name and background as props*/}
+                        <TouchableOpacity
                             color='#FFFFFF'
                             backgroundColor='#757083'
                             style={styles.chatButton}
                             title="Start Chatting"
                             onPress={() =>
-                                this.props.navigation.navigate('Chat', { name: this.state.name })
+                                this.props.navigation.navigate('Chat', {
+                                    name: this.state.name,
+                                    background: this.state.background,
+                                })
                             }
-                        />
+                        >
+                            <Text style={styles.buttonText}>Start Chatting</Text>
+                        </TouchableOpacity>
                     </View>
-                </View >
-            </ImageBackground >
-
-        )
+                </ImageBackground >
+            </View >
+        );
     }
 }
 
 const styles = StyleSheet.create({
-    container2: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: "flex",
-        backgroundColor: "white",
-        height: "44%",
-        width: "88%",
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        position: "relative",
     },
-    container3: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: "flex",
-        backgroundColor: "white",
+    contentBox: {
+        flex: 1,
+        height: "44%",
+        backgroundColor: "#ffffff",
         width: "88%",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        position: "absolute",
+        bottom: 20,
     },
     nameInput: {
-        borderColor: 'gray',
+        borderColor: "gray",
         borderWidth: 1,
         height: 60,
         width: 300,
         fontSize: 16,
-        fontWeight: '300',
+        fontWeight: "300",
         opacity: 50,
-        color: '#757083',
+        color: "#757083",
     },
     chatButton: {
-        fontWeight: '600',
-        fontSize: 16,
-        color: '#FFFFFF',
-        backgroundColor: '#757083',
-        borderColor: '#757083',
+        width: "78%",
+        height: 55,
+        backgroundColor: "#757083",
+        color: "#ffffff",
+        alignItems: "center",
+        fontWeight: "bold",
+        justifyContent: "space-evenly",
+        width: "88%",
     },
     background: {
         flex: 1,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'flex-end'
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "flex-end",
     },
     appTitle: {
         fontSize: 45,
-        fontWeight: '600',
+        fontWeight: "600",
         color: "#FFFFFF",
     },
     icon: {
@@ -104,23 +142,36 @@ const styles = StyleSheet.create({
         display: "flex",
     },
     circle1: {
-        flex: 25,
+        backgroundColor: "#090C08",
         width: 50,
-        borderRadius: 25
+        height: 50,
+        borderRadius: 25,
+        margin: 7,
     },
     circle2: {
-        flex: 25,
+        backgroundColor: "#474056",
         width: 50,
-        borderRadius: 25
+        borderRadius: 25,
+        margin: 7,
     },
     circle3: {
-        flex: 25,
+        backgroundColor: "#8A95A5",
         width: 50,
-        borderRadius: 25
+        borderRadius: 25,
+        margin: 7,
     },
     circle4: {
-        flex: 25,
+        backgroundColor: "#B9C6AE",
         width: 50,
-        borderRadius: 25
-    }
-})
+        borderRadius: 25,
+        margin: 7,
+    },
+    backgroundColorChoices: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+    },
+    buttonText: {
+        color: "#ffffff",
+        fontWeight: "bold",
+    },
+});
