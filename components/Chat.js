119c6bb11a2f5cf4fc29d8ae3ae533ed42c140b6
Drxl95 <robertu.dev@gmail.com>
2021-11-06 20:22:30 -0500

    added msgs, keyboard fix, and GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
 
 export default class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
+    }
     componentDidMount() {
         //put  username in navigation bar (passes prop from start)
         const name = this.props.route.params.name;
@@ -15,12 +22,63 @@ export default class Chat extends React.Component {
             },
             headerTintColor: "#212224",
         });
+        //contains system message welcoming user to chat
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+                {
+                    _id: 2,
+                    text: `Welcome to the chat, ${name}!`,
+                    creeatedAt: new Date(),
+                    system: true,
+                },
+            ]
+        })
+    }
+    //allows messages to be sent/submitted
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
+    //creates a bubble for the messagesa
+    renderBubble(props) {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        background: 'linear-gradient(to right, #662121, #CF2C2C)'
+                    },
+                }}
+            />
+        );
     }
 
     render() {
+
         return (
-            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                <Text>Hello Screen2!</Text>
+            <View style={{ flex: 1 }}>
+                {/* bubble around messages*/}
+                <GiftedChat
+                    renderBubble={this.renderBubble.bind(this)}
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
+                />
+                {/*Fixees android keyboard issue*/}
+                {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
             </View>
         );
     }
